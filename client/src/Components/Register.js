991cd4b66b3dc613fee3b6487f6b782f104c53d3
mahadevbsec <gsmahadeva52@gmail.com>
2025-09-14 18:59:26 +0530

    Update Register.js

--- client/src/Components/Register.js
+++ client/src/Components/Register.js
@@ -20,7 +20,7 @@ const Register = () => {
 
     try {
       const response = await fetch(
-        "http://localhost:5000/auth/register",
+        "https://racipe-sharing.onrender.com/auth/register",
         {
           method: "POST",
           headers: { "Content-Type": "application/json" },
